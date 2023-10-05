ca23629dc6c0f9c00c5b2671158e30c94a83bf97
Nils <nils.a687@gmail.com>
2023-10-05 09:38:40 +0200

    switched to rate def with dot log

--- paper/functions.py
+++ paper/functions.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.linalg import expm
 from tqdm.notebook import tqdm
 from scipy.sparse import csr_array, coo_array
+from scipy.special import factorial
 
 #### Nummerical (new)
 
@@ -108,6 +109,31 @@ def get_dyn_v2(R, i_zero, N_t,N_x, ntau, end_states):
     
     return p, one_time_p
 
+def get_non_delayed_prop(x_s, force, D,dt,dx, N_border=None):
+    N_x = len(x_s)
+    R_abs = np.zeros(( N_x, N_x))
+    
+    mx_s = np.arange(x_s[0]-dx/2,x_s[-1]+dx/2+dx/4,dx)
+    
+    
+    F = force(mx_s)
+    lp = D / dx**2 * np.exp((F*dx/D)/2)  # r_i->i+1
+    ln = D / dx**2 * np.exp(-(F*dx/D)/2)  # r_i+1->i
+    
+    R_abs[np.arange(0,N_x),np.arange(0,N_x)] = -(lp[1:]+ln[:-1]) # -(r_i->i+1 + r_i->i-1) ????
+    R_abs[np.arange(0,N_x-1),np.arange(1,N_x)] = ln[1:-1]
+    R_abs[np.arange(1,N_x),np.arange(0,N_x-1)] = lp[1:-1]
+    prop_abs = expm(R_abs*dt, )
+    return prop_abs
+
+def get_non_delayed_dyn(R, i_zero, N_t,N_x):
+    p = np.zeros((N_t,N_x), dtype = float)
+    p[0,i_zero] = 1.
+    for i in tqdm(range(1,N_t), leave=False):
+        p[i] = R@p[i-1]    
+    return p
+    
+
 #### Analytical
 
 def get_p_x4_short_time(x,k,tau,s):
@@ -123,16 +149,45 @@ def get_x2_var_short_time(tau,k,s):
     return s**2/(2*k)*(1+k*tau)
 
 
+# Theory
+
+# WRONG !!!
+def summand(t,l1,l2, tau,k = 1): 
+    return (-k)**(l1+l2) / (factorial(l1)*factorial(l2)) * \
+        (1/(l1 + 1) * (t-l1*tau)**(l1+1) * (t-l2*tau)**l2 + \
+         1/(l2 + 1) * (t-l2*tau)**(l2+1) * (t-l1*tau)**l1)
+
+# WRONG !!!!!
+def get_theo_var(t, tau, D, k = 1):
+    l1 = np.arange(0,t/tau)
+    l2 = np.arange(0,t/tau)
+    return 2*D*(np.sum(summand(t, l1[:,None], l2[None,:], tau, k = k)))
+get_theo_var = np.vectorize(get_theo_var, excluded=(1,2))
+
+def l(k, tau, t, max_p=40):
+        i = np.arange(0, max_p, 1)
+        return np.sum((-k) ** i / factorial(i) * (t - i * tau) ** i * np.heaviside(t - i * tau, 1))
+l = np.vectorize(l, )
+
+def get_theo_var_l(ts, tau, D, k = 1):
+    dt = ts[1]-ts[0]
+    max_p = np.max(ts)/tau
+    l_data = l(k,tau,ts, max_p)
+    var = np.zeros(len(ts))
+    var[1:] = (2*D*np.cumsum(l_data**2) *dt)[:-1]
+    return var
+
+
 
 #### Simulation
 def simulate_traj(N_p, N_loop, N_t, ntau, s, dt, border, force):
     pos = np.empty((N_loop,N_p,N_t))
     vel = s*np.random.randn(N_loop,N_p,N_t)*1/np.sqrt(dt)
 
-    pos[:,:,:ntau+1] = -border
-    vel[:,:,:ntau+1] = 0
+    pos[:,:,:ntau] = -border
+    vel[:,:,:ntau] = 0
 
-    for i in tqdm(range(ntau+1,N_t), leave=False):
+    for i in tqdm(range(ntau,N_t), leave=False):
         pos[:,:,i] = pos[:,:,i-1] + vel[:,:,i-1]*dt
         vel[:,:,i] += force(pos[:,:,i-ntau])
     return pos
@@ -150,5 +205,20 @@ def get_var_hist(hists, x_s):
         return np.sum(p*x_s**2 - (p*x_s)**2)
     else:
         assert "Wrong number of dim in hists"
+        
+        
+def get_steady_mean(data, i = None, max_err = 1, thresh = 0.1, min_states = 5):
+    if i is None:
+        i = len(data)-1
+    ref_var = data[i]
+    test_points = data[i::-1]
+    mask = np.cumprod(np.abs(test_points - ref_var) < ref_var*thresh).astype(bool)
+    if np.sum(mask) < min_states:
+        return False
+    steady_points = test_points[mask]
+    relerr = np.std(steady_points)/np.sqrt(len(steady_points)) / np.mean(steady_points)
+    if relerr < max_err:
+        return np.mean(steady_points), np.std(steady_points)/np.sqrt(len(steady_points))
+    return False
         
         
